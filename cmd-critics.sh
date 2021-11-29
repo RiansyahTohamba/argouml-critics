43076e6ede8ff80ce6be8654053c59ec81cc0c43
riansyah <mriansyah93@gmail.com>
2021-11-30 07:34:56 +0800

    tambah lagi jumlah sampel class utk diteliti

---
+++ cmd-critics.sh
@@ -0,0 +1,14 @@
+java -jar ~/JCodeOdor-Launcher-1.0-standalone.jar \
+-source org \
+-lib /usr/lib/jvm/java-8-openjdk-amd64/ ~/.gradle/ ~/.m2 \
+-output tahap1.sqlite -JV 1.8
+
+
+# java -jar ~/JCodeOdor-Launcher-1.0-standalone.jar \
+# -source src/org/argouml/ \
+# src/org/tigris/ src/Acme/ src/org/omg ../argouml-core-model/src/ ../argouml-core-model-euml/src/ ../argouml-core-model-mdr/src/ \
+# src/tudresden \
+# -lib /usr/lib/jvm/java-8-openjdk-amd64/ ~/.gradle/ ~/.m2 \
+# src/org/omg/lib \
+# -output test.sqlite -JV 1.8
+
