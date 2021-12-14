54ea24506d8dbdf89258fcd577c565641f28f7fe
riansyah <mriansyah93@gmail.com>
2021-12-15 06:07:07 +0800

    rename sh for generate metric

--- generate-metric.sh
+++ generate-metric.sh
@@ -1,7 +1,7 @@
 java -jar ~/JCodeOdor-Launcher-1.0-standalone.jar \
 -source src/ \
 -lib /usr/lib/jvm/java-8-openjdk-amd64/ ~/.gradle/ ~/.m2 \
--output 1-tahap-0-argouml.sqlite -JV 1.8
+-output 2-tahap-1-argouml.sqlite -JV 1.8
 
 #1-tahap-0-argouml.sqlite
 #2-tahap-1-argouml.sqlite
